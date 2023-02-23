2c1e5863d569e9b9ba66077416fa745a85cbb278
Suleiman Olaide <olaide.suleiman96@gmail.com>
2023-02-23 19:35:44 +0100

    Update FKs

--- db/migrate/20230203070356_create_users.rb
+++ db/migrate/20230203070356_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :users
       t.string :photo, limit: 1000
       t.text :bio
       t.integer :posts_counter, default: 0
--- db/migrate/20230203074700_create_likes.rb
+++ db/migrate/20230203074700_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :author, references: :users, null: false, foreign_key: { to_table: :users }
-      t.references :post, references: :posts, null: false, foreign_key: { to_table: :posts }
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
       t.timestamps
     end
   end
